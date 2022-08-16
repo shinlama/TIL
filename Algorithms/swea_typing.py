cd7d6771f9356170aebb5160e95e45a950923b9a
shinlama <jiwon.shinnn@gmail.com>
2022-08-16 15:49:03 +0900

    0816

--- Algorithms/swea_typing.py
+++ Algorithms/swea_typing.py
@@ -3,14 +3,14 @@ T = int(input())
 for tc in range(1, T+1):
     A, B = map(str, input().split())
 
+    idx = 0
     cnt = 0
-    length = 0
     for i in range(len(A)-len(B)+1):
-        if A[cnt:cnt+len(B)] == B:
-            length += 1
-            cnt = cnt + len(B) - 1
-        cnt += 1
+        if A[idx:idx+len(B)] == B:
+            cnt += 1
+            idx = idx + len(B) - 1
+        idx += 1
 
-    result = len(A) - ((len(B)-1) * length)
+    result = len(A) - ((len(B)-1) * cnt)
 
     print(f'#{tc} {result}')
